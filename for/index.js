dd1d55e66dc9a723b5ef61a3a283ca9cb21c7cd3
RokasKontrimas <rokas.kontrimas123@gmail.com>
2024-03-04 09:41:42 +0200

    loop task

---
+++ for/index.js
@@ -0,0 +1,64 @@
+function multiplyNumbers2() {
+    console.groupCollapsed('*2')
+    for (let i = 1; i <= 100; i++) {
+        console.log(i * 2);
+    }
+    console.groupEnd()
+}
+
+multiplyNumbers2()
+
+function multiplyNumbers5() {
+    console.groupCollapsed('*5')
+
+    for (let i = 1; i <= 100; i++) {
+        console.log(i * 5);
+    }
+    console.groupEnd()
+}
+
+multiplyNumbers5()
+
+function addNumbers5() {
+    console.groupCollapsed('+5')
+
+    for (let i = 1; i <= 100; i++) {
+        console.log(i + 5);
+    }
+    console.groupEnd()
+}
+
+addNumbers5()
+
+function subtractNumbers2() {
+    console.groupCollapsed('-2')
+
+    for (let i = 1; i <= 100; i++) {
+        console.log(i - 2);
+    }
+    console.groupEnd()
+}
+
+subtractNumbers2()
+
+function squareNumbers() {
+    console.groupCollapsed('* i')
+
+    for (let i = 1; i <= 100; i++) {
+        console.log(i * i);
+    }
+    console.groupEnd()
+}
+
+squareNumbers()
+
+function powNumbers() {
+    console.groupCollapsed('pow')
+
+    for (let i = 1; i <= 100; i++) {
+        console.log(Math.pow(i, 3));
+    }
+    console.groupEnd()
+}
+
+powNumbers()
