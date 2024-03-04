e9ace4479f9b6c4b3fb5f89bc0220a891159c2d1
RokasKontrimas <rokas.kontrimas123@gmail.com>
2024-03-04 10:28:05 +0200

    task 9

--- for/math.js
+++ for/math.js
@@ -1,66 +1,78 @@
-function multiplyNumbers2() {
+function multiplyNumbers2(num1, num2, num3) {
     console.groupCollapsed('*2')
-    for (let i = 100; i >= 1; i--) {
-        console.log(`${i} * 2  = ${i * 2}`);
+    for (let i = num1; i >= num2; i--) {
+        if (i === num3) {
+            console.log(`${i} * 2  = ${i * 2}`);
+        }
     }
     console.groupEnd()
 }
 
-multiplyNumbers2()
+multiplyNumbers2(30, 25, 26)
 
-function multiplyNumbers5() {
+function multiplyNumbers5(num1, num2, num3) {
     console.groupCollapsed('*5')
 
-    for (let i = 100; i >= 1; i--) {
-        console.log(`${i} * 5  = ${i * 5}`);
+    for (let i = num1; i >= num2; i--) {
+        if (i === num3) {
+            console.log(`${i} * 5  = ${i * 5}`);
+        }
     }
     console.groupEnd()
 }
 
-multiplyNumbers5()
+multiplyNumbers5(30, 25, 26)
 
-function addNumbers5() {
+function addNumbers5(num1, num2, num3) {
     console.groupCollapsed('+5')
 
-    for (let i = 100; i >= 1; i--) {
-        console.log(`${i} + 5  = ${i + 5}`);
+    for (let i = num1; i >= num2; i--) {
+        if (i === num3) {
+            console.log(`${i} + 5  = ${i + 5}`);
+        }
     }
     console.groupEnd()
 }
 
-addNumbers5()
+addNumbers5(30, 25, 26)
 
-function subtractNumbers2() {
+function subtractNumbers2(num1, num2, num3) {
     console.groupCollapsed('-2')
 
-    for (let i = 100; i >= 1; i--) {
-        console.log(`${i} - 2  = ${i - 2}`);
+    for (let i = num1; i >= num2; i--) {
+        if (i === num3) {
+            console.log(`${i} - 2  = ${i - 2}`);
+        }
     }
     console.groupEnd()
 }
 
-subtractNumbers2()
+subtractNumbers2(30, 25, 26)
 
-function squareNumbers() {
+function squareNumbers(num1, num2, num3) {
     console.groupCollapsed('* i')
 
-    for (let i = 100; i >= 1; i--) {
-        console.log(`${i} * ${i}  = ${i * i}`);
+    for (let i = num1; i >= num2; i--) {
+        if (i === num3) {
+            console.log(`${i} * ${i}  = ${i * i}`);
+        }
     }
     console.groupEnd()
 }
 
-squareNumbers()
+squareNumbers(30, 25, 26)
 
-function powNumbers() {
+function powNumbers(num1, num2, num3) {
     console.groupCollapsed('pow')
 
-    for (let i = 100; i >= 1; i--) {
-        console.log(`${i}^3 = ${Math.pow(i, 3)}`);
+    for (let i = num1; i >= num2; i--) {
+        if (i === num3) {
+            console.log(`${i}^3 = ${Math.pow(i, 3)}`);
+        }
     }
     console.groupEnd()
 }
 
-powNumbers()
+powNumbers(30, 25, 26)
 
 
